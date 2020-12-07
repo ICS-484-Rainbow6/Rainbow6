80d555bfa1dab444e36d998dccd34bc4503999d1
tianhuizhou <tianhuizhou@users.noreply.github.com>
2020-12-06 15:52:50 -1000

    update

--- pages/about.py
+++ pages/about.py
@@ -1,7 +1,6 @@
 import dash_core_components as dcc
 import dash_html_components as html
 from app import app
-
 layout = html.Div([
 
     html.Div([
@@ -19,8 +18,11 @@ layout = html.Div([
         html.Div([
             html.H3('Captain Dandy !', style={'font-family': 'sans-serif', 'line-height': '60%'}),
             html.P('I am a very long very long very very very long sentence'),
-            html.P('Please put you personal information at here here here here...')
-        ], className='six columns')
+            html.P('Please put you personal information at here here here here...'),
+            html.A([
+                html.Img(src=app.get_asset_url('github.svg'), style={'height': '10%', 'width': '10%'})
+            ], href="#")
+        ], className='six columns', style={})
     ], className='row'),
     ], className='seven columns', style={'background': 'rgba(43, 43, 43, 0.8)', 'padding-top': '20px', 'padding-bottom':'20px'}),
 

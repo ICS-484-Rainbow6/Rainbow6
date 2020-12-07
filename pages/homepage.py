b17dee46372a92b7513a88b54cb099d7734ca8fc
tianhuizhou <tianhuizhou@users.noreply.github.com>
2020-12-07 00:58:14 -1000

    complete homepage
    homepage is finished

--- pages/homepage.py
+++ pages/homepage.py
@@ -3,18 +3,19 @@ import dash_html_components as html
 from dash.dependencies import Input, Output
 
 from app import app
-#'font': '60px R6S-Light, sans-serif',
+
 layout = html.Div([
     html.Div([
-        html.H2('OPERATION NEON', style={'font-family': 'sans-serif', 'line-height': '60%'}),
-        html.H2('DAWN', style={'font-family': 'sans-serif', 'line-height': '60%', 'padding-bottom': '20px'}),
-        html.P('If you see a laser, be warned. Nighthaven was'),
-        html.P('hiding her well, but not anymore. Operation Neon'),
-        html.P('Dawn is welcoming a newcomer: Aruni. Don’t'),
-        html.P('underestimate her. Her prosthetic arm and leg'),
-        html.P('make her a fierce opponent.'),
+        html.H2('DATA ', style={'font-family': 'sans-serif', 'line-height': '60%'}),
+        html.H2('HELP YOU WIN', style={'font-family': 'sans-serif', 'line-height': '60%', 'padding-bottom': '20px'}),
+        html.P('If you are a new player,', style={'font-weight': 'bold', 'font-size': 'large'}),
+        html.P('No Worries, We got you back.'),
+        html.P('Using our amazing tools to help you find appropriate Operators and Weapon combos.'),
+        html.P('If you are already a master,', style={'font-weight': 'bold', 'font-size': 'large', 'padding-top':'15px'}),
+        html.P('Let explore more details about the game by using our tools,'),
+        html.P('and find more interesting ideas to punish your opponent in each game.'),
+
     ], style={'padding': '25px'}),
-    html.A('User Guide', href='/pages/page1', className='button', style={'margin': '25px', 'color': 'white'})
+    html.A('User Guide', href='/pages/page1', className='button', style={'margin': '18px', 'color': 'white'})
 ], className='four columns',
     style={'background': 'rgba(43, 43, 43, 0.5)', 'color': 'white', 'margin-top': '200px', 'margin-left': '300px'})
-

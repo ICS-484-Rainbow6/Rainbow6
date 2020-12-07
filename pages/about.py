efd0067657429c72d43d03dbd2b0cffaf5b82b86
tianhuizhou <tianhuizhou@users.noreply.github.com>
2020-12-06 15:19:20 -1000

    finished basic UI

--- pages/about.py
+++ pages/about.py
@@ -1,15 +1,41 @@
 import dash_core_components as dcc
 import dash_html_components as html
-
-
 from app import app
-#'font': '60px R6S-Light, sans-serif',
+
 layout = html.Div([
 
     html.Div([
-        html.H1("About Us", style={'font-family': 'Helvetica',
-                                                             "margin-top": "25",
+        html.H1("About Team", style={'font-family': 'Helvetica',
+                                                             "margin-top": "0",
                                                              "margin-bottom": "0"}, className='eight columns'),
     ], className='row'),
-    html.P("I don't know what to say but we have to say something here.")
-],  className='ten columns offset-by-one', style={'opacity': '0.955'})
+    html.P("We are the Team 666 doing data visualization for the Video game Rainbow six siege."),
+    html.Div([
+    html.Div([
+        html.Div([
+            html.Img(src=app.get_asset_url('dandyy.png'), style={'height': '100%', 'width': '100%'})
+
+        ], className='four columns', style={'margin-right': '20px', 'margin-left': '20px'}),
+        html.Div([
+            html.H3('Captain Dandy !', style={'font-family': 'sans-serif', 'line-height': '60%'}),
+            html.P('I am a very long very long very very very long sentence'),
+            html.P('Please put you personal information at here here here here...')
+        ], className='six columns')
+    ], className='row'),
+    ], className='seven columns', style={'background': 'rgba(43, 43, 43, 0.8)', 'padding-top': '20px', 'padding-bottom':'20px'}),
+
+    html.Div([
+    html.Div([
+        html.Div([
+            html.H3('Tianhui Zhou', style={'font-family': 'sans-serif', 'line-height': '60%'}),
+            html.P('I am a very long very long very very very long sentence'),
+            html.P('Please put you personal information at here here here here...')
+        ], className='six columns', style={'margin-right': '20px', 'margin-left': '20px'}),
+        html.Div([
+            html.Img(src=app.get_asset_url('Bobbyy.png'), style={'height': '100%', 'width': '100%'})
+
+        ], className='four columns')
+    ], className='row')
+    ], className='seven columns', style={'background': 'rgba(43, 43, 43, 0.8)', 'padding-top': '20px', 'padding-bottom':'20px', 'margin-left':'440px','margin-top':'20px'})
+
+],  className='ten columns offset-by-one', style={'background': 'rgba(43, 43, 43, 0)', 'color': 'white'})

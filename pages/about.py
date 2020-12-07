09807720be2308a31811d9be0485e15a451cb215
tianhuizhou <tianhuizhou@users.noreply.github.com>
2020-12-06 22:58:04 -1000

    update

--- pages/about.py
+++ pages/about.py
@@ -20,7 +20,10 @@ layout = html.Div([
             html.P('I am a very long very long very very very long sentence'),
             html.P('Please put you personal information at here here here here...'),
             html.A([
-                html.Img(src=app.get_asset_url('github.svg'), style={'height': '10%', 'width': '10%'})
+                html.Img(src=app.get_asset_url('github.png'), style={'height': '11%', 'width': '11%'})
+            ], href="#"),
+            html.A([
+                html.Img(src=app.get_asset_url('linkedin.png'), style={'height': '11%', 'width': '11%', 'margin-left':'10px'})
             ], href="#")
         ], className='six columns', style={})
     ], className='row'),
@@ -31,13 +34,42 @@ layout = html.Div([
         html.Div([
             html.H3('Tianhui Zhou', style={'font-family': 'sans-serif', 'line-height': '60%'}),
             html.P('I am a very long very long very very very long sentence'),
-            html.P('Please put you personal information at here here here here...')
+            html.P('Please put you personal information at here here here here...'),
+            html.A([
+                html.Img(src=app.get_asset_url('github.png'), style={'height': '11%', 'width': '11%'})
+            ], href='https://tianhuizhou.github.io/'),
+            html.A([
+                html.Img(src=app.get_asset_url('linkedin.png'),
+                         style={'height': '11%', 'width': '11%', 'margin-left': '10px'})
+            ], href="#")
         ], className='six columns', style={'margin-right': '20px', 'margin-left': '20px'}),
         html.Div([
             html.Img(src=app.get_asset_url('Bobbyy.png'), style={'height': '100%', 'width': '100%'})
 
         ], className='four columns')
     ], className='row')
-    ], className='seven columns', style={'background': 'rgba(43, 43, 43, 0.8)', 'padding-top': '20px', 'padding-bottom':'20px', 'margin-left':'440px','margin-top':'20px'})
+    ], className='seven columns', style={'background': 'rgba(43, 43, 43, 0.8)', 'padding-top': '20px', 'padding-bottom':'20px', 'margin-left':'440px','margin-top':'20px'}),
+
+    html.Div([
+        html.Div([
+            html.Div([
+                html.Img(src=app.get_asset_url('mybad.png'), style={'height': '100%', 'width': '100%'})
+
+            ], className='four columns', style={'margin-right': '20px', 'margin-left': '20px'}),
+            html.Div([
+                html.H3('Ray Tang', style={'font-family': 'sans-serif', 'line-height': '60%'}),
+                html.P('Hi guys, I am Jui-Chen Tang, feel free to call me Ray. '),
+                html.P('Currently I am UH student working on my second degree major in computer science focus on data science.'),
+                html.A([
+                    html.Img(src=app.get_asset_url('github.png'), style={'height': '11%', 'width': '11%'})
+                ], href="https://github.com/mybad812/mybad812.github.io"),
+                html.A([
+                    html.Img(src=app.get_asset_url('linkedin.png'),
+                             style={'height': '11%', 'width': '11%', 'margin-left': '10px'})
+                ], href="#")
+            ], className='six columns', style={})
+        ], className='row'),
+    ], className='seven columns',
+        style={'background': 'rgba(43, 43, 43, 0.8)', 'padding-top': '20px', 'padding-bottom': '20px', 'margin-top':'20px'})
 
 ],  className='ten columns offset-by-one', style={'background': 'rgba(43, 43, 43, 0)', 'color': 'white'})
